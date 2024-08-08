3b6b44f8f2296c1157ee0826eee45a3c427d12f9
Gabriel <gabrieldantaslp@gmail.com>
2024-08-08 12:39:00 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,33 @@
-import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
-  return <Button className="m-2">Test</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Gabriel!</h2>
+        <p>Quinta-feira, 05 de Agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            className="object-cover"
+            src="/banner-01.png"
+            fill
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
