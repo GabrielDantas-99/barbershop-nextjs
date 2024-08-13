389e72b75f310dffd8a9332c0d667383f56b4d4a
Gabriel <gabrieldantaslp@gmail.com>
2024-08-13 20:49:48 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import BookingItem from "./_components/booking-item"
 import Search from "./_components/search"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
@@ -43,8 +45,18 @@ export default async function Home() {
     <div>
       <Header />
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Gabriel!</h2>
-        <p>Quinta-feira, 05 de Agosto</p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user ? session.user.name : "bem vindo"}!
+        </h2>
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd", { locale: ptBR })}
+          </span>
+          <span>&nbsp;de&nbsp;</span>
+          <span className="capitalize">
+            {format(new Date(), "MMMM", { locale: ptBR })}
+          </span>
+        </p>
 
         {/* BUSCA */}
         <div className="mt-6">
