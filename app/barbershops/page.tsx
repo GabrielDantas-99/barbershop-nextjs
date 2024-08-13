40e7f71a971eac4bb20f9a821aeb02a2a3e334e8
Gabriel <gabrieldantaslp@gmail.com>
2024-08-13 00:14:15 -0300

    feat: ensure barbershop can be searched by title and service

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -6,20 +6,37 @@ import { db } from "../_lib/prisma"
 interface BarbershopsPageParams {
   searchParams: {
     title?: string
+    service?: string
   }
 }
 
 const BarbershopsPage = async ({ searchParams }: BarbershopsPageParams) => {
   const barbershops = await db.barbershop.findMany({
     where: {
-      name: {
-        contains: searchParams?.title,
-        mode: "insensitive",
-      },
+      OR: [
+        searchParams?.title
+          ? {
+              name: {
+                contains: searchParams?.title,
+                mode: "insensitive",
+              },
+            }
+          : {},
+        searchParams.service
+          ? {
+              services: {
+                some: {
+                  name: {
+                    contains: searchParams.service,
+                    mode: "insensitive",
+                  },
+                },
+              },
+            }
+          : {},
+      ],
     },
   })
-
-  console.log(searchParams)
 
   return (
     <div>
@@ -29,7 +46,8 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageParams) => {
       </div>
       <div className="px-5">
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Resultados para &quot;{searchParams.title}&quot;
+          Resultados para &quot;{searchParams?.title || searchParams?.service}
+          &quot;
         </h2>
         <div className="grid grid-cols-2 gap-4">
           {barbershops.map((barbershop) => (
